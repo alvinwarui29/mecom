8374b25824e87b2f1a353ba937372d301ca39af0
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-10-30 06:44:38 +0300

    add logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 class AdminController extends Controller
 {
     //get request
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+    //logout
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
