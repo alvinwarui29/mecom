a00defc5d0e1629f01f1318b1b8f28a587f48128
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-11-02 15:50:08 +0300

    finish user

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        $notification = array(
+            'message' => 'Login Successfully',
+            'alert-type' => 'success'
+        );
         $url ='';
         if($request->user()->role === 'admin'){
             $url = "/admin/dashboard";
@@ -38,7 +42,7 @@ class AuthenticatedSessionController extends Controller
             $url = "/dashboard";
         }
 
-        return redirect()->intended($url);
+        return redirect()->intended($url)->with($notification);
     }
 
     /**
